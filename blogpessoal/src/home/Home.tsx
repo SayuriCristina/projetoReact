4487acea42d87fdee9537aa4ba5b345ff120c495
Sayuri Cristina <sayuri.cristinass@gmail.com>
2024-06-21 13:02:49 -0300

    Cria um projeto em React

---
+++ blogpessoal/src/home/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1 className='titulo'>Home </h1>
+            <img className='foto' src='https://c8.alamy.com/comp/DR9PKD/black-guy-showing-thumbs-up-all-on-white-background-DR9PKD.jpg'></img>
+            <p> ↖ esse é um home.</p>
+        </div>
+    )
+}
+
+export default Home
