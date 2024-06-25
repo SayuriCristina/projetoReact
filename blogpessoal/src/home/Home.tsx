6683cee445944efde9ea2fbafe72db0ca6373f16
SayuriCristina <sayuri.cristinass@gmail.com>
2024-06-25 13:18:52 -0300

    Adiciona o Tailwind.

--- blogpessoal/src/home/Home.tsx
+++ blogpessoal/src/home/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import React from 'react'
 import './Home.css'
 
 interface myProps {
@@ -20,12 +21,12 @@ function Home(props: myProps) {
     return (
         <div>
 
-            <h1 className='titulo'>{props.titulo}</h1>
+            <h1 className='text-3xl font-bold underline text-violet-900'>{props.titulo}</h1>
             <img className='foto' src='https://c8.alamy.com/comp/DR9PKD/black-guy-showing-thumbs-up-all-on-white-background-DR9PKD.jpg'></img>
             <p>{props.texto}</p>
 
             <h2>{contador}</h2>
-            <button onClick={somar}>Clique aqui</button>
+            <button onClick={somar} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Clique aqui</button>
 
         </div>
     )
